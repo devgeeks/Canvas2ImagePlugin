1d435dc0d6fd3ee059b8c61861f40a1a75c777d8
tommy-carlos williams <tommy@devgeeks.org>
2012-08-23 16:54:09 +1000

    Updated for Cordova 2.0.0

--- Canvas2ImagePlugin.js
+++ Canvas2ImagePlugin.js
@@ -7,43 +7,32 @@
 //	MIT Licensed
 //
 
-
-Canvas2ImagePlugin = function() {
+(function(gap){
 	
-}
-
-Canvas2ImagePlugin.prototype.saveImageDataToLibrary = function(successCallback, failureCallback, canvasId) {
-	// successCallback required
-	if (typeof successCallback != "function") {
-        console.log("Canvas2ImagePlugin Error: successCallback is not a function");
-        return;
-    }
-	if (typeof failureCallback != "function") {
-        console.log("Canvas2ImagePlugin Error: failureCallback is not a function");
-        return;
-    }
-	var canvas = document.getElementById(canvasId);
-	var imageData = canvas.toDataURL().replace(/data:image\/png;base64,/,'');
-	if (typeof PhoneGap !== "undefined") {
-		PhoneGap.exec(successCallback, failureCallback, "Canvas2ImagePlugin","saveImageDataToLibrary",[imageData]);
-	} else if (typeof Cordova !== "undefined") {
-		Cordova.exec(successCallback, failureCallback, "Canvas2ImagePlugin","saveImageDataToLibrary",[imageData]);
+	Canvas2ImagePlugin = function() {
+		
 	}
-};
-
-if (typeof PhoneGap !== "undefined") {
-	PhoneGap.addConstructor(function(){
+	
+	Canvas2ImagePlugin.prototype.saveImageDataToLibrary = function(successCallback, failureCallback, canvasId) {
+		// successCallback required
+	    	if (typeof successCallback != "function") {
+	        	console.log("Canvas2ImagePlugin Error: successCallback is not a function");
+	        	return;
+	    	}
+	    	if (typeof failureCallback != "function") {
+	       		console.log("Canvas2ImagePlugin Error: failureCallback is not a function");
+	        	return;
+	    	}
+		var canvas = document.getElementById(canvasId);
+		var imageData = canvas.toDataURL().replace(/data:image\/png;base64,/,'');
+		gap.exec(successCallback, failureCallback, "Canvas2ImagePlugin","saveImageDataToLibrary",[imageData]);
+	};
+	
+	gap.addConstructor(function(){
 		if(!window.plugins) {
 			window.plugins = {};
 		}
 		window.plugins.canvas2ImagePlugin = new Canvas2ImagePlugin();
 	});
-}
-if (typeof Cordova !== "undefined") {
-	Cordova.addConstructor(function(){
-		if(!window.plugins) {
-			window.plugins = {};
-		}
-		window.plugins.canvas2ImagePlugin = new Canvas2ImagePlugin();
-	});
-}
+	
+})(window.PhoneGap || window.Cordova || window.cordova);
