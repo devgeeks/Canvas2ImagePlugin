399953cc3bfbe691d1966c02d14ff9259ae823bd
wbt11a <wbt11a@users.noreply.github.com>
2014-08-07 11:08:58 -0400

    Update Canvas2ImagePlugin.m
    This modification saves the image into the sandboxed directory and returns the absolute path of the image.

--- src/ios/Canvas2ImagePlugin.m
+++ src/ios/Canvas2ImagePlugin.m
@@ -6,27 +6,47 @@
 //  Copyright (c) 2012 Tommy-Carlos Williams. All rights reserved.
 //	MIT Licensed
 //
+//  99% of this code developed by tommy-carlos williams and ThalesValentim
 
 #import "Canvas2ImagePlugin.h"
 #import <Cordova/CDV.h>
+#include <stdlib.h>
 
 @implementation Canvas2ImagePlugin
 @synthesize callbackId;
 
-//-(CDVPlugin*) initWithWebView:(UIWebView*)theWebView
-//{
-//    self = (Canvas2ImagePlugin*)[super initWithWebView:theWebView];
-//    return self;
-//}
+
+-(void) saveImage:(UIImage *)image withFileName:(NSString *)imageName ofType:(NSString *)extension inDirectory:(NSString *)directoryPath {
+    if ([[extension lowercaseString] isEqualToString:@"png"]) {
+        [UIImagePNGRepresentation(image) writeToFile:[directoryPath stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.%@", imageName, @"png"]] options:NSAtomicWrite error:nil];
+    } else if ([[extension lowercaseString] isEqualToString:@"jpg"] || [[extension lowercaseString] isEqualToString:@"jpeg"]) {
+        [UIImageJPEGRepresentation(image, 1.0) writeToFile:[directoryPath stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.%@", imageName, @"jpg"]] options:NSAtomicWrite error:nil];
+    } else {
+        ALog(@"Image Save Failed\nExtension: (%@) is not recognized, use (PNG/JPG)", extension);
+    }
+}
+
 
 - (void)saveImageDataToLibrary:(CDVInvokedUrlCommand*)command
 {
+    int myRand = arc4random() % 10000000;
     self.callbackId = command.callbackId;
-	NSData* imageData = [NSData dataFromBase64String:[command.arguments objectAtIndex:0]];
-	
-	UIImage* image = [[[UIImage alloc] initWithData:imageData] autorelease];	
-	UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
-	
+    NSData* imageData = [NSData dataFromBase64String:[command.arguments objectAtIndex:0]];
+    
+    UIImage* image = [[[UIImage alloc] initWithData:imageData] autorelease];
+
+    NSString * path = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSString *temp = @"ImageFile-";
+    NSString *ImageName = [temp stringByAppendingFormat:@"%d",myRand];
+    [self saveImage:image withFileName:ImageName ofType:@"png" inDirectory:path];
+    NSString *tileDirectory = [[NSBundle mainBundle] resourcePath];
+    NSString *documentFolderPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+    NSLog(@"Tile Directory: %@", tileDirectory);
+    NSLog(@"Doc Directory: %@", documentFolderPath);
+    UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+    CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString:[NSString stringWithFormat:@"%@/%@.png", documentFolderPath, ImageName]];
+    [self.webView stringByEvaluatingJavaScriptFromString:[result toSuccessCallbackString: self.callbackId]];
+
 }
 
 - (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
